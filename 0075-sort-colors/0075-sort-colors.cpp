67daff447bf4e12823594ebe44a91e7c30dfc583
Michelle Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-08-10 11:02:07 +0700

    Time: 4 ms (26.99%), Space: 8.3 MB (8.41%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,27 @@
 class Solution {
 public:
     vector<int> sortColors(vector<int>& nums) {
-        for (int i = 0; i < nums.size() - 1; i++){
-            for (int j = nums.size() - 1; j > i; j--){
-                if (nums[j] < nums[j - 1]){
-                    int temp = nums[j];
-                    nums[j] = nums[j - 1];
-                    nums[j - 1] = temp;
-                }
+        int lower = 0;
+        int upper = nums.size() - 1;
+        int i = 0;
+        while (i <= upper && i >= lower && lower <= upper){
+            //if(nums[i] == nums[lower] || nums[i] == nums[upper]){
+            //      i++;
+            //}
+            if (nums[i] == 0 && i != lower){
+                nums[i] = nums[lower];
+                nums[lower] = 0;
+                lower++;
+            } 
+            else if (nums[i] == 2 && i != upper){
+                nums[i] = nums[upper];
+                nums[upper] = 2;
+                upper--;
             }
+            else {
+                i++;
+            }
+            
         }
         return nums;
     }
