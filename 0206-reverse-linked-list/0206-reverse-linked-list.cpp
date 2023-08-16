71475877524309de7707a8add61707762a4e58f0
Michelle Nhung Le <133190574+michelleln@users.noreply.github.com>
2023-08-16 16:18:46 +0700

    Time: 0 ms (100%), Space: 8.5 MB (14.25%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,21 +8,17 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ //Recursive
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         if (head == NULL || head->next == NULL){
             return head;
         }
-        ListNode *prev = NULL;
-        ListNode *curr = head;
-
-        while (curr != NULL){
-            ListNode *temp = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = temp;
-        }
-        return prev;
+        ListNode* prev = NULL;
+        ListNode* newHead = reverseList(head->next);
+        head->next->next = head;
+        head->next = prev;
+        return newHead;
     }
 };
