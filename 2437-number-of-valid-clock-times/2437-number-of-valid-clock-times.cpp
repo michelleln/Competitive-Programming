2ebca25416db66569bb511d019a2ffb62045b119
Michelle Nhung Le <133190574+michelleln@users.noreply.github.com>
2024-01-18 18:55:03 -0500

    Time: 0 ms (100.00%), Space: 6.3 MB (37.75%) - LeetHub

---
+++ 2437-number-of-valid-clock-times/2437-number-of-valid-clock-times.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int countTime(string time) {
+        int ans = 1;
+        if (time[4] == '?') ans *= 10;
+        if (time[3] == '?') ans *= 6;
+
+        if (time[0] == '?' && time[1] == '?') ans *= 24;
+        else {
+            if (time[0] == '?') {
+                if (time[1] > '3') ans *= 2;
+                else ans *= 3;
+            }
+            if (time[1] == '?') {
+                if (time[0] < '2') ans *= 10;
+                else ans *= 4;
+            }
+        }
+        return ans;
+    }
+    
+};
